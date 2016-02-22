c209006f22668ea19205d85a8e9f0f13789b6669
Sri Datla <cybrchitti@gmail.com>
2016-02-22 14:04:51 +0000

    Normalised Raw Text

--- vis.js
+++ vis.js
@@ -1,17 +1,26 @@
 
-var Data = "1: (0, 0), (2, 0), (2, 2)";
+var Data = "1: (023231, 055), (211, 02323), (21515, 25151)";
 
 function init() {
+	var Text, newText = [];
 	var stage = new createjs.Stage("demoCanvas");
 	var poly = new createjs.Shape();
-	Data = Data.replace(/ /g,"");
-	Data = Data.split(':');
-	Data = Data.replace( /(\(([\d]+,[\d]+)\),)+/ , '($1,$2)');
-	console.log(Data);
+	readTextFile("guards.pol",function(data){
+		Text = data;
+	});
+
+	Text = Text.replace(/ /g,"").split('\n');
+	Text.forEach( function(element, index, callback1) {
+		callback1[index] = element.replace(/\d:/,'').replace(/\(([\d]+),([\d]+)\)/g,'$1H$2').split(',');
+		callback1[index].forEach( function(element1, index, callback2) {
+			callback2[index] = element1.split('H');
+		});
+	});
+	console.log(Text);
 	stage.update();
 }
 
-function readTextFile(file)
+function readTextFile(file,callback)
 {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
@@ -22,8 +31,7 @@ function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                console.log(allText);
-                return allText;
+                callback(allText);
             }
         }
     }
