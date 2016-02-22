59cea20d4b88c6db0c61b2307d45d34ee0c5c58b
vickyd885 <vickyd885@gmail.com>
2016-02-22 14:14:49 +0000

    added basic polygon

--- vis.js
+++ vis.js
@@ -1,5 +1,4 @@
 
-var Data = "1: (023231, 055), (211, 02323), (21515, 25151)";
 
 function init() {
 	var Text, newText = [];
@@ -16,7 +15,8 @@ function init() {
 			callback2[index] = element1.split('H');
 		});
 	});
-	console.log(Text);
+	console.log(Text[0]);
+    addNodes(stage,Text[0]);
 	stage.update();
 }
 
@@ -36,4 +36,52 @@ function readTextFile(file,callback)
         }
     }
     rawFile.send(null);
+}
+
+function addNodes(stage,ideal_list){
+  var polygon = new createjs.Shape();
+  polygon.graphics.beginStroke("black");
+  var scaled = ideal_list.forEach(function(element,index,callback){
+    callback[index][0] = element[0]*100;
+    callback[index][1] = element[1]*100;
+  });
+  polygon.graphics.beginFill("Red").drawPolygon(200,200,ideal_list);
+  //polygon.graphics.lineTo(60, 60).lineTo(30, 90).lineTo(0, 60);
+  stage.addChild(polygon)
+}
+
+
+// library function to draw polygon!
+(createjs.Graphics.Polygon = function(x, y, points) {
+    this.x = x;
+    this.y = y;
+    this.points = points;
+}).prototype.exec = function(ctx) {
+    var start = this.points[0];
+    ctx.moveTo(start.x, start.y);
+    this.points.slice(1).forEach(function(point) {
+        ctx.lineTo(point.x, point.y);
+    });
+    ctx.lineTo(start.x, start.y);
+}
+createjs.Graphics.prototype.drawPolygon = function(x, y, args) {
+    var points = [];
+    if (Array.isArray(args)) {
+        args.forEach(function(point) {
+            point = Array.isArray(point) ? {x:point[0], y:point[1]} : point;
+            points.push(point);
+        });
+    } else {
+        args = Array.prototype.slice.call(arguments).slice(2);
+        var x = null;
+        args.forEach(function(val) {
+            if (x == null) {
+                x = val;
+            } else {
+                points.push({x: x, y: val});
+                x = null;
+            }
+        });
+    }
+    return this.append(new createjs.Graphics.Polygon(x, y, points));
 }
